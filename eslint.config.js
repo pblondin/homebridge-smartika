89e0b7bccd6b6a0f771187575a9c9dcf3576cb43
Philippe Blondin <philippe.blondin-veilleux@radio-canada.ca>
2026-02-01 19:42:03 -0500

    Add LICENSE, .npmignore, and ESLint config for NPM publishing

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js';
+
+export default [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            globals: {
+                console: 'readonly',
+                process: 'readonly',
+                Buffer: 'readonly',
+                __dirname: 'readonly',
+                __filename: 'readonly',
+                require: 'readonly',
+                module: 'readonly',
+                exports: 'readonly',
+                setTimeout: 'readonly',
+                clearTimeout: 'readonly',
+                setInterval: 'readonly',
+                clearInterval: 'readonly'
+            }
+        },
+        rules: {
+            'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+            'no-console': 'off',
+            'semi': ['error', 'always'],
+            'quotes': ['error', 'single']
+        }
+    },
+    {
+        files: ['src/**/*.js', 'tools/**/*.js'],
+        languageOptions: {
+            sourceType: 'commonjs'
+        }
+    }
+];
